b5f7fa5963f8b4f154d65d2bc3956867045702f8
Stefan Paul Noack <stefan@noack.software>
2018-04-07 11:51:43 +0200

    add missing exports

--- src/app/shared/component/index.ts
+++ src/app/shared/component/index.ts
@@ -2,10 +2,12 @@ export { SmdPaginatorComponent } from './smd-paginator';
 export { SmdContextualDatatableButtonComponent,
          SmdDatatableActionButtonComponent,
          SmdDataTableCellDirective,
+         SmdDataTableClickableDirective,
          SmdDataTableColumnComponent,
          SmdDataTableComponent,
          SmdDatatableDialogChangeValueComponent,
          SmdDatatableHeaderComponent,
+         SmdDatatableTitleComponent,
          SmdDataTableRowComponent } from './smd-datatable';
 export { SmdFabSpeedDialActionsComponent, SmdFabSpeedDialComponent, SmdFabSpeedDialTriggerComponent } from './smd-fab-speed-dial';
 export { SmdBottomNavComponent, SmdBottomNavGroupComponent, SmdBottomNavLabelDirective } from './smd-bottom-nav';
